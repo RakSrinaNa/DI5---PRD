b117e81e242d39aed36e12bf18a1a14dd596025c
Thomas Couchoud <zerderr@gmail.com>
2019-01-09 14:55:04 +0100

    Add module info

---
+++ Simulator/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 2019-01-09.
+ *
+ * @author Thomas Couchoud
+ * @since 2019-01-09
+ */
+module fr.mrcraftcod.simulator {
+	requires annotations;
+	
+	requires org.slf4j;
+	requires org.apache.logging.log4j;
+	requires org.apache.commons.lang3;
+	requires json;
+	requires google.or.tools;
+}
