884f077a8badb4a949929192028cbf03221eb08c
Thomas Couchoud <zerderr@gmail.com>
2019-01-23 15:48:07 +0100

    Attempt PMD
    Former-commit-id: 3925735689cc5bbef881d7cf471c0b1e309c4bf1

---
+++ Simulator/pmd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget -O pmd.zip https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.10.0/pmd-bin-6.10.0.zip
+unzip pmd.zip
+cd pmd-*
+PMD_RUNNER=$(pwd)
+cd ..
+${PMD_RUNNER}/run.sh pmd -cache target/pmd.cache -d src/main/java -f codeclimate -R rulesets/rules.xml > target/pmd.json
