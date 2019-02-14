258b966086985a80bcaa707578896ba72b0c0344
Thomas Couchoud <zerderr@gmail.com>
2019-02-14 11:15:16 +0100

    Remove resources not needed anymore
    Former-commit-id: 1b193a6e7f7a4793f17455fa57a2783f12ccf8a7

--- Simulator/pmd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-wget -O pmd.zip https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.11.0/pmd-bin-6.11.0.zip
-unzip pmd.zip
-cd pmd-*
-PMD_RUNNER=$(pwd)
-cd ..
-${PMD_RUNNER}/bin/run.sh pmd -failOnViolation false -cache target/pmd.cache -d src/main/java -f codeclimate -R rulesets/rules.xml > target/pmd.json
